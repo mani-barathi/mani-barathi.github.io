dbdbe3fabd2e6efdd5bd09690b6dd9843f374d27
mani-barathi <manibarathi.s@gmail.com>
2020-11-15 19:53:13 +0530

    fixed project icons

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ const menuBars = document.getElementsByClassName('bar');
 // Projects
 const projectItems = document.querySelectorAll('.project-item');
 
-function animateIntroText(){
+function animateIntroText() {
     let greetingTextEl = document.querySelector('.greeting-text');
     let nameTextEl = document.querySelector('.name-text');
     let nameText = nameTextEl.textContent;
     let splitNameText = nameText.split("");
 
-    greetingTextEl.style.opacity='1';
-    nameTextEl.style.opacity='1';
+    greetingTextEl.style.opacity = '1';
+    nameTextEl.style.opacity = '1';
 
     nameTextEl.textContent = '';
-    for(let i=0;i<splitNameText.length ;i++){
-        nameTextEl.innerHTML += "<span>" + splitNameText[i] +"</span>";
+    for (let i = 0; i < splitNameText.length; i++) {
+        nameTextEl.innerHTML += "<span>" + splitNameText[i] + "</span>";
     }
 
     let count = 0;
-    let timer = setInterval(animateLetterByLetter,50);
-    function animateLetterByLetter(){
+    let timer = setInterval(animateLetterByLetter, 50);
+    function animateLetterByLetter() {
         const span = nameTextEl.querySelectorAll('span')[count];
         span.classList.add('fade');
         count++;
-        if (count == splitNameText.length){
+        if (count == splitNameText.length) {
             clearInterval(timer);
             timer = null;
             return
@@ -43,7 +43,7 @@ function toggleNavbar(event) {
         navContainer.classList.replace('nav-container-slide-top', 'nav-container-slide-bottom');
     }
     navContainer.classList.toggle('navbar-active');
-    [...menuBars].forEach((bar, i) => {                        // to toggle the X mark
+    [...menuBars].forEach((bar, i) => {                        // to toggle the Hamburger Menu
         bar.classList.toggle(`bar${i + 1}-change`);
     });
 }
@@ -51,23 +51,23 @@ function toggleNavbar(event) {
 
 // Intersection observer options 
 const appearOptions = {
-    threashold:1,
-    rootMargin:"0px 0px -100px 0px",
+    threashold: 1,
+    rootMargin: "0px 0px -100px 0px",      // the bottom boundary is pushed 100px inwards to the screen
 };
 
 // Intersection observer to observe projects such that when they intersect the bottom boundary on the page, 
-// the styling of the project are updated
-const appearOnScroll = new IntersectionObserver((entries,appearOnScroll)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
+// the styling of the project are updated to give a sliding-in illusion
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
             entry.target.classList.add('project-item-active');
             appearOnScroll.unobserve(entry.target);
         }
     });
-},appearOptions);
+}, appearOptions);
 
 // telling intersection observer to observe the projects
-projectItems.forEach(project=>{
+projectItems.forEach(project => {
     appearOnScroll.observe(project);
 });
 
